995cbf7c2cae6cbc8aba1dcb91975ea5b0ad5cca
AAngeloB <asvpangelob@gmail.com>
2018-03-07 14:19:04 -1000

    Angelo

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-var test = ["one, two, three, four"]
+var test = ["one, two, three, four"];
 var word = "Angelo";
 
-function arrayMethod(){
-	
+console.log(test);
 
+
+document.getElementById().innerHTML = test;
+test.pop();
+document.getElementById().innerHTML = test.push(word);
+
+var text = "";
+var a;
+for (a = 0; a < 3; a++) {
+	text += test[a] + "<br>";
 
 }
+document.getElementById("fourth").innerHTML = text;
+
+var me = {
+	firstname: "Angelo",
+	lastname: "Barangan",
+	age: 16
+}
+document.getElementById("fourth").innerHTML
